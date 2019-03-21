312fa51be25671bfdc2ca0830c25f0a6aaaacbf1
Ryan Leyva <rleyva@shelterinsurance.com>
2019-03-21 15:45:00 -0500

    Adding Project Structure

---
+++ src/main/kotlin/com/leyva/geometry/GeometryCalculatorApplication.kt
@@ -0,0 +1,12 @@
+package com.leyva.geometry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeometryCalculatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<GeometryCalculatorApplication>(*args)
+}
+
