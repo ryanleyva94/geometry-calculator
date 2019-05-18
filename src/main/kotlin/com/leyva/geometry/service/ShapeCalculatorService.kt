7360663282e54e66c0697e151feb3e42bb991246
Ryan Leyva <ryan.c.leyva@gmail.com>
2019-05-18 17:15:51 -0500

    Added some rounding logic

--- src/main/kotlin/com/leyva/geometry/service/ShapeCalculatorService.kt
+++ src/main/kotlin/com/leyva/geometry/service/ShapeCalculatorService.kt
@@ -5,7 +5,7 @@ import com.leyva.geometry.model.RectangularPrism
 class ShapeCalculatorService{
 
     fun calculateRectangularVolume(rectangularPrism: RectangularPrism): Double{
-        return rectangularPrism.width * rectangularPrism.height * rectangularPrism.depth
+        return Math.round(rectangularPrism.width * rectangularPrism.height * rectangularPrism.depth * 1000.0)/1000.0
     }
 
     fun calculateRectangularServiceArea(rectangularPrism: RectangularPrism): Double{
@@ -13,7 +13,7 @@ class ShapeCalculatorService{
         var faceTwo = 2 * rectangularPrism.width * rectangularPrism.depth
         var faceThree = 2 * rectangularPrism.depth * rectangularPrism.height
 
-        return faceOne + faceTwo + faceThree
+        return Math.round((faceOne + faceTwo + faceThree) * 1000.0)/1000.0
     }
 
 
